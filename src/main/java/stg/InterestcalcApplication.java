dd896baef28d212069b7b5e445e8b18c7dcb6952
Kevin McCann <kevin.j.mccann@gmail.com>
2017-03-01 16:00:59 -0500

    initial commit

---
+++ src/main/java/stg/InterestcalcApplication.java
@@ -0,0 +1,12 @@
+package stg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterestcalcApplication.class, args);
+	}
+}
