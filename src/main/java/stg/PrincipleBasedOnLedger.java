50028cbee5f10bf63140c04920dc07f93d251bb9
Andy Kim <andykim@zipcoders-MacBook-Pro.local>
2017-03-02 16:54:56 -0500

    all methods done, awaiting some refactoring

---
+++ src/main/java/stg/PrincipleBasedOnLedger.java
@@ -0,0 +1,76 @@
+package stg;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+
+public class PrincipleBasedOnLedger {
+
+    Account account = new Account();
+
+    long getPrincipleAtTimeOfPayment(int Payment){
+        return account.getBalance();
+    }
+
+     Integer getAveragePrinciple(ArrayList<Integer> ledger, int period){
+
+        Integer principle = 0;
+
+        int startingIndex = ledger.size() - period;
+        for(int i = startingIndex; i < ledger.size(); i++){
+            principle += ledger.get(i);
+
+        }
+        return  principle/period;
+    }
+
+   public long getBalanceAtSetDate(int daysPriorToInterest){
+
+         ///this method will call a getter from Rick's class to return a particular balance for the day
+       return 0;
+   }
+
+    public Long returnMaxPrinciples(ArrayList<Long> ledger, int period){
+        List<Long> newList = ledger.subList(ledger.size()-period, ledger.size());
+        Collections.sort(newList);
+        return newList.get(newList.size()-1);
+
+    }
+
+    public Long returnMinPrinciples(ArrayList<Long> ledger, int period){
+        List<Long> newList = ledger.subList(ledger.size()-period, ledger.size());
+        Collections.sort(newList);
+        return newList.get(0);
+
+    }
+
+    public long returnMinBalanceOverNumberOfDay(ArrayList<Long> ledger, int days){
+
+        ArrayList<Long> consecutiveDaysBalance = new ArrayList<>();
+        for(int i = 0; i <= ledger.size()-days ; i++){
+            if(ledger.get(i).equals(ledger.get(i+days -1))){
+
+                consecutiveDaysBalance.add(ledger.get(i));
+            }
+
+        }
+
+        Collections.sort(consecutiveDaysBalance);
+        return consecutiveDaysBalance.get(0);
+    }
+
+    public long returnMaxBalanceOverNumberOfDay(ArrayList<Long> ledger, int days){
+
+        ArrayList<Long> consecutiveDaysBalance = new ArrayList<>();
+        for(int i = 0; i <= ledger.size()-days ; i++){
+            if(ledger.get(i).equals(ledger.get(i+days -1))){
+
+                consecutiveDaysBalance.add(ledger.get(i));
+            }
+
+        }
+
+        Collections.sort(consecutiveDaysBalance);
+        return consecutiveDaysBalance.get(consecutiveDaysBalance.size()-1);
+    }
+}
---
+++ src/test/java/stg/PrincipleBasedOnLedgerTest.java
@@ -0,0 +1,109 @@
+package stg;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by andykim on 3/2/17.
+ */
+public class PrincipleBasedOnLedgerTest {
+
+    PrincipleBasedOnLedger principleBasedOnLedger;
+
+
+    @Before
+    public void setUp(){
+        principleBasedOnLedger = new PrincipleBasedOnLedger();
+
+    }
+
+    @Test
+    public void getAvgPrincipleTest(){
+        ArrayList<Integer> ledger = new ArrayList<>();
+        ledger.add(100);
+        ledger.add(300);
+        ledger.add(400);
+        ledger.add(200);
+        ledger.add(600);
+        Integer expected = 320;
+        Integer actual = principleBasedOnLedger.getAveragePrinciple(ledger, 5);
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void getMaxPrincipleOverPeriod(){
+        ArrayList<Long> ledger = new ArrayList<>();
+        ledger.add((long)200);
+        ledger.add((long)800);
+        ledger.add((long)600);
+        ledger.add((long)400);
+
+        Long actual = principleBasedOnLedger.returnMaxPrinciples(ledger, 3);
+        Long expected = (long) 800;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void getMinPrincipleOverPeriod(){
+        ArrayList<Long> ledger = new ArrayList<>();
+        ledger.add((long)200);
+        ledger.add((long)300);
+        ledger.add((long)600);
+        Long actual = principleBasedOnLedger.returnMinPrinciples(ledger, 3);
+        Long expected = (long) 200;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public  void returnMinBalanceOverNumberOfDay(){
+
+        ArrayList<Long> ledger = new ArrayList<>();
+        ledger.add((long)200);
+        ledger.add((long)200);
+        ledger.add((long)200);
+        ledger.add((long)100);
+        ledger.add((long)100);
+        ledger.add((long)300);
+        ledger.add((long)600);
+        ledger.add((long)600);
+        ledger.add((long)600);
+        ledger.add((long)600);
+        ledger.add((long)600);
+        ledger.add((long)600);
+        ledger.add((long)600);
+
+        long actual = principleBasedOnLedger.returnMinBalanceOverNumberOfDay(ledger, 7);
+        long expected = 600;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public  void returnMaxBalanceOverNumberOfDay(){
+
+        ArrayList<Long> ledger = new ArrayList<>();
+        ledger.add((long)200);
+        ledger.add((long)200);
+        ledger.add((long)200);
+        ledger.add((long)100);
+        ledger.add((long)100);
+        ledger.add((long)800);
+        ledger.add((long)800);
+        ledger.add((long)300);
+        ledger.add((long)600);
+        ledger.add((long)600);
+        ledger.add((long)700);
+        ledger.add((long)700);
+
+
+        long actual = principleBasedOnLedger.returnMaxBalanceOverNumberOfDay(ledger, 2);
+        long expected = 800;
+        assertEquals(expected, actual);
+
+    }
+}
