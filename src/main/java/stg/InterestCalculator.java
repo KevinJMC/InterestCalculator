6003dc060f84ff65a99547c1b790b0f74722fadc
Kevin McCann <kevin.j.mccann@gmail.com>
2017-03-02 15:21:55 -0500

    junit added to pom

--- src/main/java/stg/InterestCalculator.java
+++ src/main/java/stg/InterestCalculator.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class InterestCalculator {
-    long calculateSimpleInterest(Account account, int interval) {
-        return account.getBalance() * interval * account.getInterestRate();
-    }
+    long calculateSimpleInterest(Account account, int interval) {}
 
     long calculateComplexInterest(Account account, int interval, int frequency) {}
 }
