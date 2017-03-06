a5b19a1569bc6e86185fb8a979f146c08eae8074
rjackson88 <richardwjacksonjr@yahoo.com>
2017-03-06 01:16:05 -0500

    Finished Account, AccountRepository and AccountController

--- src/main/java/stg/controller/account/AccountController.java
+++ src/main/java/stg/controller/account/AccountController.java
@@ -1,13 +1,20 @@
 package stg.controller.account;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import stg.exception.AccountNotFoundException;
+import stg.exception.InvalidAccountRequestException;
 import stg.model.account.Account;
 import stg.repository.account.AccountRepository;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * Created by rickjackson on 3/6/17.
@@ -37,5 +44,34 @@ public class AccountController {
                                       isMinimumBalanceRequired);
         accountRepository.save(account);
         return account;
+    }
+    
+    @RequestMapping(value = "/account/{accountId}", method = RequestMethod.GET)
+    public Account getAccount(@PathVariable("accountId") Long accountId) {
+        if (accountId == null) {
+            throw new InvalidAccountRequestException();
+        }
+        Account account = accountRepository.findOne(accountId);
+        
+        if (account == null) {
+            throw new AccountNotFoundException();
+        }
+        return account;
+    }
+    
+    @RequestMapping(value = "/account/{accountId}", method = RequestMethod.GET)
+    public List<Account> getAccounts() {
+        return (List<Account>) accountRepository.findAll();
+    }
+    
+    @RequestMapping(value = "/account/{accountId}",
+                    method = RequestMethod.DELETE)
+    public void removeAccount(@PathVariable("accountId") long accountId,
+                              HttpServletResponse httpResponse) {
+        if (accountRepository.exists(accountId)) {
+            Account account = accountRepository.findOne(accountId);
+            accountRepository.delete(account);
+        }
+        httpResponse.setStatus(HttpStatus.NO_CONTENT.value());
     }
 }
---
+++ src/main/java/stg/exception/InvalidAccountRequestException.java
@@ -0,0 +1,8 @@
+package stg.exception;
+
+/**
+ * Created by rickjackson on 3/6/17.
+ */
+public class InvalidAccountRequestException extends RuntimeException {
+    
+}
